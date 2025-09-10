d426ca30cea8c223d0a28a1bb33b9de16f99efaa
Raithser <m.adella@ui.ac.id>
2025-09-10 19:05:48 +0700

    bismillah

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "m-adella-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "<https://m-adella-footballnews.pbp.cs.ui.ac.id/>"
+    "https://m-adella-footballnews.pbp.cs.ui.ac.id/"
 ]
 # Application definition
 
